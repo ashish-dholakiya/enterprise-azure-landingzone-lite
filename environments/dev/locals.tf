6f2667afcc6866bbbdf68fd7b892b8978a83e0e7
Ashish Dholakiya <ashish.d@live.com>
2026-05-12 13:21:57 +0530

    Fix dev variables and common tags

--- environments/dev/locals.tf
+++ environments/dev/locals.tf
@@ -3,12 +3,14 @@ locals {
 
   resource_prefix = "adlz"
 
-  common_tags = merge(
-    var.tags,
-    {
-      deployed_by = "github-actions"
-    }
-  )
+  common_tags = {
+    environment = "dev"
+    project     = "enterprise-azure-landingzone-lite"
+    managed_by  = "terraform"
+    owner       = "ashish-dholakiya"
+    cost_center = "personal-lab"
+    deployed_by = "github-actions"
+  }
 
   resource_group_name = "rg-${local.resource_prefix}-dev-${local.location_short}-01"
 }
--- environments/dev/variables.tf
+++ environments/dev/variables.tf
@@ -2,8 +2,3 @@ variable "location" {
   description = "Azure region for the Dev environment"
   type        = string
 }
-
-variable "location" {
-  description = "Azure region for the Dev environment"
-  type        = string
-}
