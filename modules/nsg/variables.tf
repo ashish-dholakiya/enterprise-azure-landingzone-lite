c64a5ddb718d7d5bbba72a724d68cec2d0a9d732
Ashish Dholakiya <ashish.d@live.com>
2026-05-13 17:11:20 +0530

    Add reusable NSG module

---
+++ modules/nsg/variables.tf
@@ -0,0 +1,19 @@
+variable "name" {
+  description = "Name of the Network Security Group"
+  type        = string
+}
+
+variable "location" {
+  description = "Azure region"
+  type        = string
+}
+
+variable "resource_group_name" {
+  description = "Resource group name"
+  type        = string
+}
+
+variable "tags" {
+  description = "Tags for the NSG"
+  type        = map(string)
+}
